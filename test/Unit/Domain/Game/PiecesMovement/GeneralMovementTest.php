3e2b2fac884af808c0517134daaa2ea4c40c71bb
Milko Kosturkov <mkosturkov@gmail.com>
2025-08-20 12:31:54 +0300

    WIP: Implement tests for not allowing checkmate after next move

--- test/Unit/Domain/Game/PiecesMovement/GeneralMovementTest.php
+++ test/Unit/Domain/Game/PiecesMovement/GeneralMovementTest.php
@@ -5,6 +5,7 @@ namespace Mkosturkov\Chess\Test\Domain\Game\PiecesMovement;
 use Mkosturkov\Chess\Domain\ChessGame\Color;
 use Mkosturkov\Chess\Domain\ChessGame\Piece;
 use Mkosturkov\Chess\Domain\ChessGame\PieceType;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Mkosturkov\Chess\Domain\ChessGame\Board;
 use Mkosturkov\Chess\Domain\ChessGame\File;
@@ -29,16 +30,6 @@ class GeneralMovementTest extends TestCase
         $this->assertFalse($this->isMoveAllowed($board, $from, $from));
     }
 
-    public function test_can_not_move_if_going_to_lead_to_mate()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function test_can_not_move_anything_but_to_escape_mate()
-    {
-        $this->markTestIncomplete();
-    }
-
     public function test_can_not_move_if_not_on_turn()
     {
         $board = new Board();
@@ -46,5 +37,27 @@ class GeneralMovementTest extends TestCase
         $to = new Position(File::A, Rank::Three);
         $board = $board->withPiece($from, new Piece(Color::White, PieceType::Pawn));
         $this->assertFalse($this->isMoveAllowed($board, $from, $to, Color::Black));
+    }
+
+    #[DataProvider('mateMovesProvider')]
+    public function test_can_not_move_if_not_escaping_chess($expected, $from, $to)
+    {
+        $board = new Board()
+            ->withPiece(new Position(File::D, Rank::Four), new Piece(Color::White, PieceType::King))
+            ->withPiece(new Position(File::C, Rank::Six), new Piece(Color::White, PieceType::Rook))
+            // a black bishop is in front of a white king and diagonal of white rook
+            ->withPiece(new Position(File::D, Rank::Five), new Piece(Color::Black, PieceType::Bishop))
+            // the black pawn is next to the black bishop and in front of the white rook, giving chess to the white king
+            ->withPiece(new Position(File::C, Rank::Five), new Piece(Color::Black, PieceType::Pawn));
+
+        $this->assertEquals($expected, $this->isMoveAllowed($board, $from, $to));
+    }
+
+    public static function mateMovesProvider(): array
+    {
+        return [
+            'king moves left (mate by bishop)' => [false, new Position(File::D, Rank::Four), new Position(File::C, Rank::Four)],
+            'rook moves right (mate by pawn)' => [false, new Position(File::C, Rank::Six), new Position(File::D, Rank::Six)],
+        ];
     }
 }
